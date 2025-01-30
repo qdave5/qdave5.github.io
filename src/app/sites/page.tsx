c22ce960cd3b7bb48d78b994ee60f6b5a2716ba2
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2025-01-30 17:17:17 +0700

    feat: new page and page layout

--- src/app/sites/page.tsx
+++ src/app/sites/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Site() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -20,7 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">Save and see your changes instantly.</li>
+          <li>Also, this is a site page.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -49,11 +50,11 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="/sites"
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Go to Sites
+            Back to Home
           </a>
         </div>
       </main>
