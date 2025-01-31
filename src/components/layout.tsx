efd466991456b6c9e2b15fb4c1f7f9d0353a2e26
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2025-01-31 11:03:40 +0700

    feat: update layout page, home page, and add footer, navbar, intro and site box

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,9 @@ export default function PageLayout({
       <header>
         <Navbar />
       </header>
-      <main>{children}</main>
+      <div className="items-center justify-items-center min-h-screen p-8 pt-32 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        {children}
+      </div>
       <PageFooter />
     </>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,19 +10,29 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
+        <Link
+          href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+            />
+          </svg>
+
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            qdave5
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
